22e894c3cda121f5ad1dd65210196817f3f3d9f0
Híthalo César <hithalocesar@gmail.com>
2022-03-30 17:31:34 -0300

    Update instructions

--- Média aritmética de números racionais.py
+++ Média aritmética de números racionais.py
@@ -1,8 +1,9 @@
 # Calcula a média aritmética de três números racionais
 
+print("Os números inseridos devem ser racionais e o separador decimal, se presente, deve ser um ponto.\n")
 while True:
     while True:
-        x1 = input("Insira o primeiro número racional: ")
+        x1 = input("Insira o primeiro número: ")
         print("")
         try:
             x1 = float(x1)
@@ -11,7 +12,7 @@ while True:
         if type(x1) == float:
             break
     while True:
-        x2 = input("Insira o segundo número racional: ")
+        x2 = input("Insira o segundo número: ")
         print("")
         try:
             x2 = float(x2)
@@ -20,7 +21,7 @@ while True:
         if type(x2) == float:
             break
     while True:
-        x3 = input("Insira o terceiro número racional: ")
+        x3 = input("Insira o terceiro número: ")
         print("")
         try:
             x3 = float(x3)
